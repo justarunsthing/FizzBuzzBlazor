6396d92a80276656c4fa3eeeaf9e881d5bc2f916
justarunsthing <justarunsthing@outlook.com>
2025-10-28 08:21:16 +0000

    Added FizzBuzzModel

---
+++ Models/FizzBuzzModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FizzBuzzBlazor.Models
+{
+    public class FizzBuzzModel
+    {
+        public int FizzValue { get; set; }
+        public int BuzzValue { get; set; }
+        public int StopValue { get; set; }
+    }
+}
