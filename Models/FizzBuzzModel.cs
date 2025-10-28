ddaf1f4171203bf17436a9ce792f7635c630ad17
justarunsthing <justarunsthing@outlook.com>
2025-10-28 08:43:35 +0000

    Updated FizzBuzzModel

--- Models/FizzBuzzModel.cs
+++ Models/FizzBuzzModel.cs
@@ -1,9 +1,19 @@
-﻿namespace FizzBuzzBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzBlazor.Models
 {
     public class FizzBuzzModel
     {
-        public int FizzValue { get; set; }
-        public int BuzzValue { get; set; }
-        public int StopValue { get; set; }
+        [Required(ErrorMessage = "Please enter a Fizz value")]
+        [Range(1, 100, ErrorMessage = "Fizz value must be between 1 and 100")]
+        public int FizzValue { get; set; } = 3;
+
+        [Required(ErrorMessage = "Please enter a Buzz value")]
+        [Range(1, 100, ErrorMessage = "Buzz value must be between 1 and 100")]
+        public int BuzzValue { get; set; } = 5;
+
+        [Required(ErrorMessage = "Please enter a Stop value")]
+        [Range(1, 1000, ErrorMessage = "Stop value must be between 1 and 1000")]
+        public int StopValue { get; set; } = 100;
     }
 }
