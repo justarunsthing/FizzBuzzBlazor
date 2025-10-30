bbf3121a3ece0ac248cfd79a8d8a54875291c2c7
justarunsthing <justarunsthing@outlook.com>
2025-10-30 08:30:19 +0000

    Added FizzBuzzValidator class

---
+++ Components/Validators/FizzBuzzValidator.cs
@@ -0,0 +1,11 @@
+﻿using FizzBuzzBlazor.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace FizzBuzzBlazor.Components.Validators
+{
+    // Non visual component, needs to inherit from ComponentBase in order to be used in razor files
+    public class FizzBuzzValidator : ComponentBase
+    {
+    }
+}
